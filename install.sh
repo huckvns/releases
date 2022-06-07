3e5caf09ba4ede5438890b898d0d07b8e16c16da
Nikita Yasnogorodskyi <nikita@orca.security>
2022-06-07 04:59:56 +0300

    feat: add install script for linux/macos

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+OS="$(uname -s)"
+ARCH="$(arch)"
+mkdir -p ~/HuckVns/answers
+cd ~/HuckVns
+declare LINK
+
+case "$OS" in
+Linux | *BSD)
+	LINK="https://github.com/huckvns/releases/releases/download/v1.0.0/huckvns-linux-x64"
+	;;
+Darwin)
+	case "$ARCH" in
+	arm64)
+		LINK="https://github.com/huckvns/releases/releases/download/v1.0.0/huckvns-macos-arm64"
+		;;
+	x86_64)
+		LINK="https://github.com/huckvns/releases/releases/download/v1.0.0/huckvns-macos-x64"
+		;;
+	*) ;;
+	esac
+	open ~/HuckVns
+	;;
+*)
+	echo "OS $OS is not currently supported."
+	;;
+esac
+
+curl -Lo huckvns $LINK
+chmod +x huckvns
+
+echo -e "Программу успішно завантажено \n"
+echo "Додайте html файли з відповідями до папки $(realpath answers)"
+echo "Після цього запустість программу"
